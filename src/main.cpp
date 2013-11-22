c216d196afbc0ea9663020cb30c013b7b8beda38
Dralagen <dralagen@dralagen.fr>
2013-11-22 09:15:19 +0100

    fixed : constructor game(int,int) deleted

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <boost/lexical_cast.hpp>
 
 #include "Labyrinth.hpp"
 #include "Game.hpp"
@@ -8,37 +7,18 @@ using namespace std;
 
 int main(/*int argc, char **argv*/)
 {
-	Game *g;
-
-
-#ifdef BOOST_LEXICAL_CAST_INCLUDED
-
-	if (argc == 1) {
-		g = new Game();
-	}
-	else if (argc == 3) {
-		g = new Game(boost::lexical_cast<int>(argv[1]),boost::lexical_cast<int>(argv[2]));
-	}
-	else {
-		cout << "Error arg : " << argv[0] << " Width Height" << endl;
-		return 1;
-	}
-
-#else
-
 	int tailleX,tailleY;
-
+	string name;
 	cout << "Taille en Abscisse : ";
 	cin >> tailleX;
 	cout << "Taille en Ordonnée : ";
 	cin >> tailleY;
+	cout << "Nom du personnage : ";
+	cin >> name;
 
-	g=new Game(tailleX, tailleY);
+	Game g(name, tailleX, tailleY);
 
-#endif
-
-
-	g->launch();
+	g.launch();
 
 	return 0;
 }
