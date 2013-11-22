2cbe645a6e66612f0ebafbcdaa0c4204b87134d0
Dralagen <dralagen@dralagen.fr>
2013-11-22 11:44:56 +0100

    Add doc for class RoomComportement

--- src/RoomComportement.hpp
+++ src/RoomComportement.hpp
@@ -1,3 +1,10 @@
+/**
+ * \file RoomComportement.hpp
+ *
+ * \author Dralagen
+ * \version 1.0
+ * \date 22/11/2013
+ */
 #ifndef ROOMCOMPORTEMENT_HPP
 #define ROOMCOMPORTEMENT_HPP
 
@@ -11,20 +18,55 @@
 #define RC_CLEAN_MONSTER  2
 #define RC_UNDO -1
 
+/**
+ * \class RoomComportement
+ * \brief Génère le comportement des salles
+ */
 class RoomComportement
 {
 	public:
+		/**
+		 * \brief Constructeur
+		 *
+		 * \param lvl niveau de la salle
+		 */
 		RoomComportement (int lvl = 10);
+		/**
+		 * \brief Destructeur
+		 */
 		virtual ~RoomComportement();
 
+		/**
+		 * \brief Execute l'action du comportement
+		 *
+		 * \param p Personnage qui l'éxecute
+		 *
+		 * \return l'action realisé
+		 */
 		virtual int action(Personnage &p) = 0;
+		/**
+		 * \brief Vérifie la fin
+		 *
+		 * \return vrais si on est sur la salle de fin
+		 */
 		virtual bool isEnd() const;
 
+		/**
+		 * \brief Récupère le niveau ou force de la salle
+		 *
+		 * \return le niveau
+		 */
 		int getLvl();
+
+		/**
+		 * \brief Récupère le contenue de la salle
+		 *
+		 * \return contenue de la salle
+		 */
 		std::string getContent();
 	protected:
-		std::string m_content;
-		int m_lvl;
+		std::string m_content;   ///< Valeur du contenue a afficher
+		int         m_lvl;       ///< stock le niveau de la salle
 
 };
 
