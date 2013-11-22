26ef24316a10b5f59926bfc83c9cdd9b9256a100
Dralagen <dralagen@dralagen.fr>
2013-11-22 09:12:39 +0100

    Add doc in class Game

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,3 +1,11 @@
+/**
+ * \file Game.hpp
+ *
+ * \author Dralagen
+ * \version 1.0
+ * \date 22/11/2013
+ */
+
 #ifndef GAME_HPP
 #define GAME_HPP
 
@@ -17,26 +25,49 @@
 #include "DArmeC.hpp"
 #include "DArmeV.hpp"
 
+/**
+ * \class Game Gère une partie
+ *          manage un labyrinth avec un joueur
+ */
 class Game
 {
 	public:
-		Game (int x = LABYRINTH_TAILLE_X_DEFAULT,
-					int y = LABYRINTH_TAILLE_Y_DEFAULT);
-
+		/**
+		 * \brief constucteur
+		 *
+		 * \param name nom du personnage
+		 * \param x lageur
+		 * \param y hauteur
+		 */
 		Game(std::string name,
 				 int x = LABYRINTH_TAILLE_X_DEFAULT,
 				 int y = LABYRINTH_TAILLE_Y_DEFAULT);
 
+		/**
+		 * \brief Débute le déroulement d'une partie
+		 */
 		void launch();
+
+		/**
+		 * \brief Choisie une direction et execute l'action de la salle
+		 */
 		void chooseRoom();
+
+		/**
+		 * \brief Action de fin
+		 *
+		 */
 		void end();
+
+		/**
+		 * \brief Affiche les commandes
+		 */
 		void help();
 
 
 	private:
-		Labyrinth m_labyrinth;
-		Personnage m_perso;
-
+		Labyrinth m_labyrinth; ///< le labyrinthe
+		Personnage m_perso; ///< le personnage
 };
 
 #endif /* GAME_HPP */
